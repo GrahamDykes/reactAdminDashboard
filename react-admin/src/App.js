c017945383c1f5e084bd78480cebfa7945f13112
GrahamDykes <153469002+GrahamDykes@users.noreply.github.com>
2024-04-21 21:47:45 -0700

    Final Form

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -9,10 +9,10 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -33,10 +33,10 @@ function App() {
               <Route path="/invoices" element={<Invoices/>}></Route>
               <Route path="/form" element={<Form/>}></Route>
               <Route path="/bar" element={<Bar/>}></Route>
-              {/* <Route path="/pie" element={<Pie/>}></Route> */}
-              {/* <Route path="/line" element={<Line/>}></Route> */}
+              <Route path="/pie" element={<Pie/>}></Route>
+              <Route path="/line" element={<Line/>}></Route>
               <Route path="/faq" element={<FAQ/>}></Route>
-              {/* <Route path="/geography" element={<Geography/>}></Route> */}
+              <Route path="/geography" element={<Geography/>}></Route>
               <Route path="/calendar" element={<Calendar/>}></Route>
             </Routes>
           </main>
